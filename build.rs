b67a850e8cf9e5934ef5d9162ae011ea1badb566
S4ndf1re <jan.kleinmann2000@gmail.com>
2023-09-01 19:19:32 +0200

    change 100mb to githubs recommended 50mb

--- build.rs
+++ build.rs
@@ -7,8 +7,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let xac = std::fs::read("model/xac")?;
     let xad = std::fs::read("model/xad")?;
     let xae = std::fs::read("model/xae")?;
+    let xaf = std::fs::read("model/xaf")?;
+    let xag = std::fs::read("model/xag")?;
+    let xah = std::fs::read("model/xah")?;
+    let xai = std::fs::read("model/xai")?;
     
-    let data = [xaa, xab, xac, xad, xae].concat();
+    let data = [xaa, xab, xac, xad, xae, xaf, xag, xah, xai].concat();
     std::fs::write("model/model.onnx", data)?; 
 
     Ok(())
